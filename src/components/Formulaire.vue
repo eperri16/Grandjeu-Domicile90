ec5741f61dbbb9e7b15b99cbc2f40c5d6bb08ab5
Nathan Van Bignoot <nathan.vanbignoot@icloud.com>
2024-02-23 17:02:26 +0100

    refractor: adding responsiveness on GrandJeu.astro

--- src/components/Formulaire.vue
+++ src/components/Formulaire.vue
@@ -6,21 +6,23 @@
   <div class="formulaire">
     <h3>Pour participer, entrez vos informations et jouez !</h3>
     <form action="">
-      <div class="form-cat">
-        <label class="to-complete" for="lastname">Nom*</label>
-        <input type="text" id="lastname" name="lastname" required>
-      </div>
-      <div class="form-cat">
-        <label class="to-complete" for="name">Prénom*</label>
-        <input type="text" id="name" name="name" required>
-      </div>
-      <div class="form-cat">
-        <label class="to-complete" for="email">Email*</label>
-        <input type="text" id="email" name="email" required>
-      </div>
-      <div class="form-cat">
-        <label class="to-complete" for="phone-number">Numéro de téléphone</label>
-        <input type="text" id="phone-number" name="phone-number">
+      <div class="form-grid">
+        <div class="form-cat">
+          <label class="to-complete" for="lastname">Nom*</label>
+          <input type="text" id="lastname" name="lastname" required>
+        </div>
+        <div class="form-cat">
+          <label class="to-complete" for="name">Prénom*</label>
+          <input type="text" id="name" name="name" required>
+        </div>
+        <div class="form-cat">
+          <label class="to-complete" for="email">Email*</label>
+          <input type="text" id="email" name="email" required>
+        </div>
+        <div class="form-cat">
+          <label class="to-complete" for="phone-number">Numéro de téléphone</label>
+          <input type="text" id="phone-number" name="phone-number">
+        </div>
       </div>
       <div class="form-check">
         <label class="to-check" for="subscribe">En participant à ce jeu, vous acceptez de recevoir des offres d'emplois d'Amaelles et toutes informations relatives à nos actions de recrutement</label>
@@ -45,10 +47,37 @@
     padding: 0 12px;
     font-family: "Playfair Display Variable", serif;
     font-weight: 800;
+    @media screen and (min-width: $m-screen) {
+      text-align: left;
+      font-size: 20px;
+      padding: 0;
+    }
   }
   form {
     display: flex;
     flex-direction: column;
+    @media screen and (min-width: $m-screen){
+      .form-grid {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 20px;
+        align-items: center;
+        .form-cat:nth-child(1) {
+          grid-area: 1 / 1 / 2 / 4;
+        }
+        .form-cat:nth-child(2) {
+          grid-area: 1 / 4 / 2 / 7;
+        }
+        .form-cat:nth-child(3) {
+          grid-area: 2 / 1 / 3 / 5;
+        }
+        .form-cat:nth-child(4) {
+          grid-area: 2 /5 /3 /7;
+        }
+      }
+    }
   }
   .form-cat {
     display: flex;
@@ -72,7 +101,7 @@
     display: flex;
     flex-direction: row-reverse;
     align-items: flex-start;
-    justify-content: flex-start;
+    justify-content: flex-end;
     margin: 16px 0;
     label.to-check {
       font-weight: 200;
